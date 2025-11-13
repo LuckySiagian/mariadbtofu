7ecc0c7d7435eecd495327f0198a4c887bfe0544
Lucky Siagian <luckysiagian2004@gmail.com>
2025-11-13 15:31:37 +0700

    Add Kubernetes secret for MariaDB password

---
+++ secrets.tf
@@ -0,0 +1,11 @@
+resource "kubernetes_secret" "mariadb_secret" {
+  metadata {
+    name = "mariadb-secret"
+  }
+
+  data = {
+    mariadb-password = base64encode("siskaweek12")
+  }
+
+  type = "Opaque"
+}
